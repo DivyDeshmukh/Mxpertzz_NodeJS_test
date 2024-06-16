26c6899189a49c17b5aa599c24a5bb454416ca20
Divy Deshmukh <127101632+DivyDeshmukh@users.noreply.github.com>
2024-06-16 18:01:45 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'https://placementcell.vercel.app/',
+    origin: process.env.CORS_ORIGIN || 'https://placementcell-seven.vercel.app',
     credentials: true
 }));
 
